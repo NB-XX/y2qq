8702183026b3dc84ab5defc83a8875e20e1e82de
NB-XX <53631369+NB-XX@users.noreply.github.com>
2022-05-19 01:33:14 +0800

    Update y2qq.py

--- y2qq.py
+++ y2qq.py
@@ -1,23 +1,27 @@
 import os
-import subprocess
 import PySimpleGUI as sg
+import subprocess as sp
 
 print = sg.Print
 
 
-def resteam(in_youtube, in_ffmpeg, in_port, in_url, in_key):
-    youtube_path = in_youtube
-    ffmpeg_path = in_ffmpeg
+def set_proxy(in_port):
     port = in_port
-    url = in_url
-    key = in_key
     os.environ["http_proxy"] = f"http://127.0.0.1:{port}"
     os.environ["https_proxy"] = f"http://127.0.0.1:{port}"
-    print('代理设置成功')
-    get_url = os.popen(f'{youtube_path} -g {url}')
-    print('别急 正在获取m3u8')
-    m3u8 = get_url.read()
-    print('m3u8获取成功')
-    get_url.close()
-    subprocess.Popen(
-        f'{ffmpeg_path} -i {m3u8.strip()} -vcodec libx264 -acodec aac -f flv "rtmp://6721.livepush.myqcloud.com/live/{key}"')
+
+
+def get_m3u8(in_youtube, in_url):
+    youtube_path = in_youtube
+    url = in_url
+    m3u8 = os.popen(f'{youtube_path} -g {url}').read().strip()
+    return m3u8
+
+
+def resteam(m3u8, in_ffmpeg, in_key):
+    ffmpeg_path = in_ffmpeg
+    key = in_key
+    process = sp.Popen(
+        f'{ffmpeg_path} -i {m3u8} -vcodec libx264 -acodec aac -f flv "rtmp://6721.livepush.myqcloud.com/live/{key}"', stdout=sp.PIPE, stderr=sp.STDOUT, universal_newlines=True)
+    for line in process.stdout:
+        print(line)
