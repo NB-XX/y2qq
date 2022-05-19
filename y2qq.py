fe85ea6384df7c7c3b5353c4ee7c97b862653d68
NB-XX <53631369+NB-XX@users.noreply.github.com>
2022-05-20 02:33:18 +0800

    Update y2qq.py

--- y2qq.py
+++ y2qq.py
@@ -2,8 +2,6 @@ import os
 import PySimpleGUI as sg
 import subprocess as sp
 
-print = sg.Print
-
 
 def set_proxy(in_port):
     port = in_port
@@ -15,13 +13,16 @@ def get_m3u8(in_youtube, in_url):
     youtube_path = in_youtube
     url = in_url
     m3u8 = os.popen(f'{youtube_path} -g {url}').read().strip()
+    sg.cprint(f'获取m3u8成功')
     return m3u8
 
 
-def resteam(m3u8, in_ffmpeg, in_key):
+def restream(m3u8, in_ffmpeg, in_key):
+    global process
     ffmpeg_path = in_ffmpeg
     key = in_key
     process = sp.Popen(
-        f'{ffmpeg_path} -i {m3u8} -vcodec libx264 -acodec aac -f flv "rtmp://6721.livepush.myqcloud.com/live/{key}"', stdout=sp.PIPE, stderr=sp.STDOUT, universal_newlines=True)
+        [ffmpeg_path, '-i', m3u8, '-vcodec', 'libx264', '-acodec', 'aac', '-f', 'flv', f"rtmp://6721.livepush.myqcloud.com/live/{key}"], stdout=sp.PIPE, stderr=sp.STDOUT, universal_newlines=True)
     for line in process.stdout:
-        print(line)
+        if 'speed' in line:
+            sg.cprint(line.rstrip("\n"))
